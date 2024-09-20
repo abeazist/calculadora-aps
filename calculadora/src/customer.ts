e51cd4052bb5950fbd6d2aa3c5a0e3773044e96f
beatriz <128046453+abeazist@users.noreply.github.com>
2024-09-20 11:31:06 -0300

    aprendendo td

---
+++ calculadora/src/customer.ts
@@ -0,0 +1,8 @@
+//Calculadora.ts
+export default class Customer { 
+    name: string = "";
+    age: number = 0;
+    email: string = "";
+    birthDate: Date = new Date();
+    //attributes and methods here
+}
---
+++ calculadora/src/index.ts
@@ -0,0 +1,9 @@
+import Customer from "./customer";
+
+const customer1 = new Customer();
+customer1.name = "Beatriz";
+customer1.age = 18;
+customer1.email = "beatrizoliveira@gmail.com"
+
+
+console.log(customer1)
